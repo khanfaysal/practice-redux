ef244886524b2084872db58348fcbb53f2e27a5d
lincoln khan <mdfaysalkhancse@gmail.com>
2022-05-18 11:59:12 +0600

    already create action types and set actions running

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,18 @@
+
+import { ActionTypes } from "../constants/action-types";
+
+// set products action
+export const setProducts = (products) => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products
+    }
+
+}
+export const setProducts = (products) => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products
+    }
+
+}
